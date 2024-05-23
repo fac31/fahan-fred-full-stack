5b22a0ecfc1cffe274062b05f82b8d542d36e232
fredhartley <fredmhartley@gmail.com>
2024-05-23 19:41:49 +0300

    Added fetch request to news api and tweaked index.html

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
 // Import necessary modules
 import express from 'express';
 import fetch from 'node-fetch';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { dirname } from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-
-
 const app = express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.use(express.static('public'));
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get('/data', async (req, res) => {
+    try {
+        const response = await fetch(`https://newsapi.org/v2/top-headlines?country=gb&category=business&apiKey=${process.env.NEWS_API_KEY}`);
+        const data = await response.json();
+        console.log(data);
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
