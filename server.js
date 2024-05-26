14fd51889ea2c3fe9ae5ca1d91204b322f258b4d
Fahan Ibrahim-Hashi <fahan.ibrahim@gmail.com>
2024-05-26 23:30:47 +0100

    added middleware to server.js

--- server.js
+++ server.js
@@ -6,11 +6,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
-
 const app = express();
 const port = 3000;
 
+// Use CORS middleware to allow cross-origin requests
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
 app.listen(port, () => {
