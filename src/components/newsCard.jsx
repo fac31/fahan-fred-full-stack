a6b16741fc5268eda028751104d9ef11618d107e
fredhartley <fredmhartley@gmail.com>
2024-06-03 21:14:27 +0100

    removed unnecessary class name

--- src/components/newsCard.jsx
+++ src/components/newsCard.jsx
@@ -2,7 +2,7 @@ import '../index.css';
 
 export default function NewsCard({ news, number }) {
     return (
-        <div className="card hover-underline-animation">
+        <div className="card">
             <img src={news.urlToImage} alt={news.title} />
             <h2>{number}. {news.title}</h2>
             <p>{news.description}</p>
